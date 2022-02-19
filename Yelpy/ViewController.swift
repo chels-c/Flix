24490a53925b98c9ce2f0d2b2aeb7c72d6b7190b
chels-c <94879318+chels-c@users.noreply.github.com>
2022-02-19 15:16:24 -0800

    Initial Commit

---
+++ Yelpy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yelpy
+//
+//  Created by Chelsea Chu on 2/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
